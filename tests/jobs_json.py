ac32c153e0594b4663e3e68d8ba69bcc5f5ce7ee
bio353 <fabiomcn2001@gmail.com>
2022-11-30 20:50:15 -0300

    Updated tests

---
+++ tests/jobs_json.py
@@ -0,0 +1,83 @@
+import datetime
+from bson.objectid import ObjectId
+
+SAMPLE_JOBS = [
+    {
+        "_id": "6364ee60ce535cccc45406e8",
+        "date": "2022:11:04:10:50:08.474000",
+        "vacancies": 124,
+        "companies": 64,
+        "type": "mobile",
+        "technology": "React Native"
+    },
+    {
+        "_id": "6364ee6ece535cccc45406e9",
+        "date": "2022:11:04:10:50:22.177000",
+        "vacancies": 442,
+        "companies": 292,
+        "type": "web",
+        "technology": "React"
+    },
+    {
+        "_id": "6364ee7bce535cccc45406ea",
+        "date": "2022:11:04:10:50:35.746000",
+        "vacancies": 283,
+        "companies": 205,
+        "type": "backend",
+        "technology": "Node"
+    }
+]
+
+SAMPLE_JOBS_BY_TYPE = [
+    {
+        "_id": ObjectId("6364ee60ce535cccc45406e8"),
+        "date": "2022:11:04:10:50:08.474000",
+        "vacancies": 124,
+        "companies": 64,
+        "type": "mobile",
+        "technology": "React Native"
+    }
+]
+
+
+EXPECTED_JOBS = [
+    {
+        "name": "React Native",
+        "vagas": [
+            {
+                "x": 124,
+                "y": "04"
+            }
+        ]
+    },
+    {
+        "name": "React",
+        "vagas": [
+            {
+                "x": 442,
+                "y": "04"
+            }
+        ]
+    },
+    {
+        "name": "Node",
+        "vagas": [
+            {
+                "x": 283,
+                "y": "04"
+            }
+        ]
+    }
+]
+
+EXPECTED_JOBS_BY_TYPE = [
+    {
+        "name": "React Native",
+        "vagas": [
+            {
+                "x": 124,
+                "y": "04"
+            }
+        ]
+    }
+]
--- tests/test_data_manipulation.py
+++ tests/test_data_manipulation.py
@@ -1,33 +1,35 @@
 import json
 from unittest import TestCase
 from database.data_manipulation import DataManipulation as DataManip
-from database.database_connection import DatabaseConnection as DBConnection
-from tests.sample_json import *
+from database.db_connection import DatabaseConnection as DBConnection
+from tests.jobs_json import *
 
 
 class TestDataManipulation(TestCase):
+    maxDiff: int | None = None
+
     def setUp(self):
-        self.db_connection_mock = DBConnection()
-        self.db_connection_mock.get_jobs_json = lambda: SAMPLE_JSON
-        self.db_connection_mock.get_jobs_json_by_type = lambda job_type: SAMPLE_JSON_BY_TYPE
-        self.data_manip = DataManip(self.db_connection_mock)
+        self.db_mock = DBConnection()
+        self.db_mock.get_jobs_json = lambda: SAMPLE_JOBS
+        self.db_mock.get_jobs_json_by_type = lambda job_type: SAMPLE_JOBS_BY_TYPE
+        self.data_manip = DataManip(self.db_mock)
+
+    def test_get_all_jobs(self):
+        job_list = self.data_manip.get_all_jobs()
+        self.assertEqual(job_list, EXPECTED_JOBS)
 
     def test_get_jobs_by_type(self):
         jobs_json = self.data_manip.get_jobs_by_type("mobile")
-        self.assertEqual(jobs_json, EXPECTED_JSON_BY_TYPE)
-
-    def test_update_json_file(self):
-        self.data_manip.update_json_file()
-        with open('jobs2.json') as file:
-            file_data = json.load(file)
-        self.assertEqual(file_data, SAMPLE_JSON)
+        self.assertEqual(jobs_json, EXPECTED_JOBS_BY_TYPE)
 
     def test_read_json_file(self):
         self.data_manip.read_json_file()
-        with open('jobs2.json') as file:
+        with open('docs/jobs.json') as file:
             file_data = json.load(file)
-        self.assertEqual(file_data, SAMPLE_JSON)
-    
-    def test_return_all_jobs(self):
-        job_list = self.data_manip.return_all_jobs()
-        self.assertEqual(job_list, EXPECTED_JSON)
+        self.assertEqual(file_data, SAMPLE_JOBS)
+
+    def test_update_json_file(self):
+        self.data_manip.update_json_file()
+        with open('docs/jobs.json') as file:
+            file_data = json.load(file)
+        self.assertEqual(file_data, SAMPLE_JOBS)
